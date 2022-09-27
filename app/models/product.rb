ab19de3c96243f474748a353462d1fe28259ab14
viktors1992 <v.nilovs@gmail.com>
2022-09-27 21:18:20 +0300

    db associations ,cart and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,23 @@
 class Product < ApplicationRecord
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+    #validations
     validates :title, uniqueness: true
+    validates :title, length: {minimum: 10}
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
     validates :image_url, allow_blank: true, format: {
         with: %r{\.(gif|jpg|png)\Z}i,
         message: 'must be a URL for GIF, JPG or PNG image.'
         }
-        
+    private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+        end
+    end
 end
