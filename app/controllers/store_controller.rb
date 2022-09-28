4b45fb2f972947dd3d2c10830b29a35f8906ea7b
viktors1992 <v.nilovs@gmail.com>
2022-09-28 16:46:09 +0300

    chapter 10,E1 and E2

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,18 @@
 class StoreController < ApplicationController
+  before_action :session_counter
+  def session_counter 
+
+      if session[:counter].nil?  #|| @line_item
+        session[:counter] = 0 
+      end
+        session[:counter] += 1      
+      @session_counter = session[:counter]
+
+
+
+
+  end
+
   def index
     @products = Product.order(:title)
   end
